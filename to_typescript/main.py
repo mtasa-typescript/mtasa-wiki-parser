2c3092b7857994cbd49f3c06560ce5abc72fe7ab
Toliak <toliakpurple@gmail.com>
2021-02-15 12:44:44 +0300

    Typescript types generator improved

--- to_typescript/main.py
+++ to_typescript/main.py
@@ -1,10 +1,12 @@
 import os
-from typing import List, Optional
+from typing import List, Optional, Dict, Set
 
 from dump.shared import DATA as SHARED_DATA
 from dump.client import DATA as CLIENT_DATA
 from dump.server import DATA as SERVER_DATA
 from src.fetch.function import CompoundFunctionData, FunctionData
+
+from to_typescript.src.function_gen import docs_string, signature_string, find_function_in_file
 
 
 # TODO: Looks like piece of script. Refactor that to readable code....
@@ -20,25 +22,6 @@ def init_workspace():
     init_dir('output/types/server')
 
 
-def prepare_type(source_type: str) -> str:
-    if source_type == 'int' or source_type == 'float' or source_type == 'uint':
-        return 'number'
-    if source_type == 'str':
-        return 'string'
-    if source_type == 'bool':
-        return 'boolean'
-    if source_type == 'bool':
-        return 'boolean'
-    if source_type == 'var':
-        return 'any'
-    if source_type == 'mixed':
-        return 'string'
-    if source_type == 'function' or source_type == 'handle' or source_type == 'callback':
-        return 'HandleFunction'
-
-    return source_type
-
-
 FILE_STARTER = dict(server='import {account, acl, aclgroup,' \
                            ' player, table, ban, blip,colshape,' \
                            'element,ped,pickup,resource,team,textdisplay,' \
@@ -50,54 +33,37 @@ FILE_STARTER = dict(server='import {account, acl, aclgroup,' \
                            'element,ped,pickup,resource,team,textdisplay,' \
                            'vehicle,xmlnode,textitem,HandleFunction,file,' \
                            'marker,radararea,request,userdata,timer,' \
-                           'water} from "mtasa/shared/structures";\n',
+                           'water,iterator} from "mtasa/shared/structures";\n',
                     client='import {account, acl, aclgroup,' \
                            ' player, table, ban, blip,colshape,' \
                            'element,ped,pickup,resource,team,textdisplay,' \
                            'vehicle,xmlnode,textitem,HandleFunction,file,' \
                            'marker,radararea,request,userdata,timer,' \
-                           'water} from "mtasa/client/structures";\n',
+                           'water,browser,progressBar,light,effect,'
+                           'gui,searchlight,weapon,guibrowser,'
+                           'txd,dff,col,ifp,primitiveType,guiscrollbar,'
+                           'guimemo,texture,objectgroup,projectile,Matrix,'
+                           '} from "mtasa/client/structures";\n',
                     )
 
-
-def docs_string(data: FunctionData) -> str:
-    docs = '    /**\n'
-    for line in data.docs.description.split('\n'):
-        docs += '     * ' + line + '\n'
-    for arg in data.signature.arguments:
-        if arg.name not in data.docs.arguments:
-            continue
-        docs += '     * @param ' + arg.name + ' ' \
-                + data.docs.arguments[arg.name].replace('\n', '  ') + '\n'
-        if arg.default_value:
-            docs += '     * @default ' + arg.default_value + '\n'
-    for line in data.docs.result.split('\n'):
-        docs += '     * @return ' + line + '\n'
-    docs += '     */\n'
-
-    return docs
+FUNCTION_BLACKLIST: Dict[str, Set[str]] = {
+    'server': {
+        '',
+    },
+    'client': {
+        '',
+    },
+    'shared': {
+        'utf8.byte',
+        'utf8.lower',
+        'utf8.match',
+    },
+}
 
 
-def signature_string(data: FunctionData) -> str:
-    signature = data.signature
-    if len(signature.return_types) == 1:
-        return_type = prepare_type(signature.return_types[0])
-    else:
-        type_list = [prepare_type(i) for i in signature.return_types]
-        return_type = f'LuaMultiReturn<[{",".join(type_list)}]>'
+def function_file_gen(data_list: List[CompoundFunctionData], key: str, dir_path: str):
+    # This function ignores Blacklist
 
-    arguments = ''
-    for arg in signature.arguments:
-        arg_name = arg.name
-        if arg_name == 'default':
-            arg_name = 'defaultValue'
-        arguments += f'{arg_name}{"?" if arg.optional and not "..." in arg.name else ""}' \
-                     f': {prepare_type(arg.argument_type)}, '
-
-    return f'    export function {signature.name}({arguments}): {return_type};\n\n'
-
-
-def typescript_functions(data_list: List[CompoundFunctionData], key: str, dir_path: str):
     with open(f'{dir_path}/function.d.ts', 'w', encoding='UTF-8') as file:
         file.write(FILE_STARTER[key])
         file.write(f"declare module 'mtasa/{key}/functions' " + "{\n\n")
@@ -118,6 +84,38 @@ def typescript_functions(data_list: List[CompoundFunctionData], key: str, dir_pa
         file.write("}")
 
 
+def function_file_replace(data_list: List[CompoundFunctionData], key: str, dir_path: str):
+    with open(f'{dir_path}/function.d.ts', 'r', encoding='UTF-8') as file:
+        file_data = file.read()
+
+    for f in data_list:
+        if f.server and 'utf8.' in f.server.signature.name:
+            continue  # skip utf8 functions
+
+        if f.server == f.client and key != 'shared':
+            print(f'[INFO] Function {f.server.signature.name} skipped, because it is Shared')
+            continue
+
+        data: Optional[FunctionData] = getattr(f, key if key != 'shared' else 'server')
+        if data.signature.name in FUNCTION_BLACKLIST[key]:
+            continue
+
+        start_pos, end_pos = find_function_in_file(data.signature.name, file_data)
+        data_to_replace = docs_string(data) + signature_string(data)
+
+        file_data = file_data[:start_pos] + data_to_replace + file_data[end_pos:]
+
+    with open(f'{dir_path}/function.d.ts', 'w', encoding='UTF-8') as file:
+        file.write(file_data)
+
+
+def typescript_functions(data_list: List[CompoundFunctionData], key: str, dir_path: str):
+    if os.path.exists(f'{dir_path}/function.d.ts'):
+        return function_file_replace(data_list, key, dir_path)
+
+    return function_file_gen(data_list, key, dir_path)
+
+
 def utf8_functions():
     with open(f'output/types/shared/utf8.d.ts', 'w', encoding='UTF-8') as file:
         file.write(FILE_STARTER['shared'])
@@ -129,10 +127,13 @@ def utf8_functions():
                 continue
 
             data: Optional[FunctionData] = f.server
+            if data.signature.name in FUNCTION_BLACKLIST['shared']:
+                continue
+
             data.signature.name = data.signature.name.replace('utf8.', '')
 
             file.write(docs_string(data))
-            file.write(signature_string(data))
+            file.write(signature_string(data) + '\n')
 
         file.write("    }\n}\n")
 
@@ -144,4 +145,4 @@ if __name__ == '__main__':
     typescript_functions(CLIENT_DATA + SHARED_DATA, 'client', 'output/types/client')
     typescript_functions(SHARED_DATA, 'shared', 'output/types/shared')
 
-    utf8_functions()
+    # utf8_functions() # TODO FIXME: fix replace
---
+++ to_typescript/src/function_gen.py
@@ -0,0 +1,95 @@
+import re
+
+from src.fetch.function import FunctionData
+
+
+def prepare_type(source_type: str) -> str:
+    TYPE_ALIASES = {
+        'gui-browser': 'guibrowser',
+        'gui-scrollbar': 'guiscrollbar',
+        'gui-memo': 'guimemo',
+        'matrix': 'Matrix',
+    }
+    if source_type == 'int' or source_type == 'float' or source_type == 'uint' or source_type == 'color':
+        return 'number'
+    if source_type == 'str':
+        return 'string'
+    if source_type == 'bool':
+        return 'boolean'
+    if source_type == 'bool':
+        return 'boolean'
+    if source_type == 'var' or source_type == 'value':
+        return 'any'
+    if source_type == 'mixed':
+        return 'string'
+    if source_type == 'function' or source_type == 'handle' or source_type == 'callback':
+        return 'HandleFunction'
+
+    if source_type in TYPE_ALIASES:
+        return TYPE_ALIASES[source_type]
+
+    return source_type
+
+
+def prepare_arg_name(name: str) -> str:
+    if name == 'default':
+        return 'defaultValue'
+    if name == 'var':
+        return 'variableValue'
+
+    return name
+
+
+def docs_string(data: FunctionData) -> str:
+    docs = '    /**\n'
+
+    for line in data.docs.description.split('\n'):
+        docs += '     * ' + line + '\n'
+    docs += '     * @see {@link https://wiki.multitheftauto.com/wiki/' + data.signature.name + '|MTASA Wiki}\n'
+
+    for arg in data.signature.arguments:
+        if arg.name not in data.docs.arguments:
+            continue
+        docs += '     * @param ' + arg.name + ' ' \
+                + data.docs.arguments[arg.name].replace('\n', '  ') + '\n'
+        if arg.default_value:
+            docs += '     * @default ' + arg.default_value + '\n'
+
+    for index, line in enumerate(data.docs.result.split('\n')):
+        if index == 0:
+            docs += '     * @return ' + line + '\n'
+        else:
+            docs += '     * * ' + line + '\n'
+
+    docs += '     */\n'
+
+    return docs
+
+
+def signature_string(data: FunctionData) -> str:
+    signature = data.signature
+    if len(signature.return_types) == 1:
+        return_type = prepare_type(signature.return_types[0])
+    else:
+        type_list = [prepare_type(i) for i in signature.return_types]
+        return_type = f'LuaMultiReturn<[{",".join(type_list)}]>'
+
+    arguments = ''
+    for arg in signature.arguments:
+        arg_name = prepare_arg_name(arg.name)
+
+        arguments += f'{arg_name}{"?" if arg.optional and not "..." in arg.name else ""}' \
+                     f': {prepare_type(arg.argument_type)}, '
+
+    return f'    export function {signature.name}({arguments}): {return_type};\n'
+
+
+def find_function_in_file(function_name: str, file_data: str) -> (int, int):
+    regex = re.compile(rf'^ *\/\*\*\n(((?<!\/\*\*)[\S\s])+?)\*\/\n.+function ({function_name})\W.+\n', re.MULTILINE)
+    result = regex.search(file_data)
+
+    if not result:
+        raise RuntimeError(f'Function definition for {function_name} not found. '
+                           f'Please provide it or remove the whole file to regenerate it')
+
+    return result.start(), result.end()
